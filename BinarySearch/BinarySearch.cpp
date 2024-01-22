d0444869eadf21ab6aa37b730ed04e2f6af8871d
modernTalker <116522860+modernTalker@users.noreply.github.com>
2024-01-22 18:37:43 +0300

    Update BinarySearch.cpp

--- BinarySearch/BinarySearch.cpp
+++ BinarySearch/BinarySearch.cpp
@@ -1,1 +1,63 @@
 
+#ifndef BOUND
+#define BOUND
+template <class T>
+bool Equal(const T& a, const T& b) {
+  return (!(a < b)) && (!(b < a));
+}
+
+template <class T>
+bool BinarySearch(const T* array, const T* end, const T& value) {
+  const T* left = array;
+  const T* right = end - 1;
+
+  while (0 < right - left) {
+    auto middle = left + (right - left) / 2;
+
+    if (*(middle) < value) {
+      left = middle + 1;
+    } else {
+      right = middle;
+    }
+  }
+  return (!Equal(left, end)) && (Equal(*(left), value));
+}
+
+template <class T>
+const T* LowerBound(const T* arr, const T* end, const T& value) {
+  const T* left = arr;
+  const T* right = end - 1;
+  while (0 < right - left) {
+    auto middle = left + (right - left) / 2;
+
+    if (*(middle) < value) {
+      left = middle + 1;
+    } else {
+      right = middle;
+    }
+  }
+  if ((left < end) && (*left < value)) {
+    ++left;
+  }
+  return left;
+}
+
+template <class T>
+const T* UpperBound(const T* arr, const T* end, const T& value) {
+  const T* left = arr;
+  const T* right = end - 1;
+  while (0 < right - left) {
+    auto middle = left + (right - left) / 2;
+    if (*middle < value) {
+      left = middle + 1;
+    } else {
+      right = middle;
+    }
+  }
+  while ((left < end) && ((*left < value) || (Equal(*left, value)))) {
+    ++left;
+  }
+  return left;
+}
+
+#endif
